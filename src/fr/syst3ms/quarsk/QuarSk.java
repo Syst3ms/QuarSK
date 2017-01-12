df7a9b13cf767d2dcca077de4e5a645da9ce3630
Syst3ms <arthmm14@gmail.com>
2017-01-12 20:02:17 +0100

    Remade the registering + new features
    As you can see, there is a static Skript import. I was just really annoyed of having to write `Skript.` each time. I'm not adding any other than this one.

--- src/fr/syst3ms/quarsk/QuarSk.java
+++ src/fr/syst3ms/quarsk/QuarSk.java
@@ -1,45 +1,175 @@
 package fr.syst3ms.quarsk;
 
-import ch.njol.skript.Skript;
-import ch.njol.skript.SkriptAddon;
-import ch.njol.skript.classes.data.SkriptClasses;
+import ch.njol.skript.classes.ClassInfo;
+import ch.njol.skript.classes.Parser;
 import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.ParseContext;
+import ch.njol.skript.registrations.Classes;
+import ch.njol.skript.util.Timespan;
+import com.google.common.collect.Iterators;
+import com.sun.istack.internal.Nullable;
+import fr.syst3ms.quarsk.conditions.CondHasPotionEffect;
 import fr.syst3ms.quarsk.effects.EffLinkReference;
 import fr.syst3ms.quarsk.effects.EffOrientTowards;
+import fr.syst3ms.quarsk.effects.EffUnlinkReference;
+import fr.syst3ms.quarsk.effects.potion.EffApplyPotionEffects;
+import fr.syst3ms.quarsk.effects.potion.EffMilkEntity;
 import fr.syst3ms.quarsk.expressions.SExprReference;
+import fr.syst3ms.quarsk.expressions.potion.*;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionData;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.potion.PotionType;
 import org.bukkit.util.Vector;
 
 import java.util.logging.Level;
 
-import static java.lang.Math.PI;
+import static ch.njol.skript.Skript.*;
+
 
 /**
  * Created by Syst3ms on 29/12/2016.
  */
 public class QuarSk extends JavaPlugin {
 
-    public static final double RAD_TO_DEG = 180 / PI;
+    public static final double RAD_TO_DEG = 180 / Math.PI;
+    private static QuarSk instance;
 
     public void onEnable() {
-        getLogger().log(Level.INFO, "There are " + Skript.getEvents().size() + " events, " + Skript.getConditions().size() + " conditions and " + Skript.getEffects().size() + " effects registered !");
-        getLogger().log(Level.INFO, "Enabling QuarSk " + this.getConfig().get("version") + " to give these numbers a boost !");
-        Skript.registerAddon(this);
-        Skript.registerEffect(EffOrientTowards.class, "(orient %entity%|(make %entity%|force %entity% to) (face|look)) (0¦towards|1¦away from) %location%");
-        Skript.registerEffect(EffLinkReference.class, "link @<\\S+> to %object%");
-        Skript.registerEffect(EffUnlinkReference.class, "unlink @<\\S+>");
-        Skript.registerExpression(SExprReference.class,Object.class, ExpressionType.SIMPLE, "@<\\S+>");
-        getLogger().log(Level.INFO, "Now, there are " + Skript.getEvents().size() + " events, " + Skript.getConditions().size() + " conditions and " + Skript.getEffects().size() + " effects registered ! Good game !");
+        instance = this;
+        getLogger().log(Level.INFO, "There are " + getEvents().size() + " events, " + getConditions().size() + " conditions, " + getEffects().size() + " effects and " + Iterators.size(getExpressions()) + " expressions registered !");
+        getLogger().log(Level.INFO, "Enabling QuarSk " + getVersion() + " to give these numbers a boost !");
+        normalRegister();
+        getLogger().log(Level.INFO, "Now, there are " + getEvents().size() + " events, " + getConditions().size() + " conditions and " + getEffects().size() + " effects and " + Iterators.size(getExpressions()) + " expressions registered ! Good game !");
     }
 
     public void onDisable() {
         getLogger().info("Disabling QuarSk 1.0 ! Goodbye !");
     }
 
+    private void normalRegister() {
+        registerAddon(this);
+        /*
+         * TYPES
+         */
+        //Potions
+        Classes.registerClass(new ClassInfo<PotionEffect>(PotionEffect.class, "potioneffect")
+                .name("potion effect")
+                .description("A getter for potion effects")
+                .parser(new Parser<PotionEffect>() {
+                            @Override
+                            @Nullable
+                            public PotionEffect parse(String obj, ParseContext context) {
+                                return null;
+                            }
+
+                            @Override
+                            public String toString(PotionEffect potionEffect, int i) {
+                                return "effect" + potionEffect.getType() + ", tier " + potionEffect.getAmplifier() + ", duration " + potionEffect.getDuration() + ", particles " + potionEffect.hasParticles() + ", ambient " + potionEffect.isAmbient() + ", color " + potionEffect.getColor();
+                            }
+
+                            @Override
+                            public String toVariableNameString(PotionEffect potionEffect) {
+                                return potionEffect.getType() + "," + potionEffect.getAmplifier() + "," + potionEffect.getDuration() + "," + potionEffect.hasParticles() + "," + potionEffect.isAmbient() + "," + potionEffect.getColor();
+                            }
+
+                            public String getVariableNamePattern() {
+                                return ".+";
+                            }
+                        }
+                )
+        );
+        /*
+         * EFFECTS
+         */
+        //Orientation
+        registerEffect(EffOrientTowards.class, "(orient %entity%|(make %entity%|force %entity% to) (face|look)) (0¦towards|1¦away from) %location%");
+        //References
+        registerEffect(EffLinkReference.class, "link @<\\S+> to %object%");
+        registerEffect(EffUnlinkReference.class, "unlink @<\\S+>");
+        registerEffect(EffApplyPotionEffects.class, "apply [potion] [effect[s] [of]] %potioneffects% to %livingentities%");
+        registerEffect(EffMilkEntity.class, "milk %livingentities%");
+        /*
+         * CONDITIONS
+         */
+        //Potions
+        registerCondition(CondHasPotionEffect.class, "[entity] %livingentity% (0¦has [got]|1¦has( not|n't) [got]) [(the|a)] %potioneffecttype% [potion] effect");
+        /*
+         * EXPRESSIONS
+         */
+        //References
+        registerExpression(SExprReference.class, Object.class, ExpressionType.SIMPLE, "@<\\S+>");
+        //Potions
+        registerExpression(ExprCustomPotionEffect.class, PotionEffect.class, ExpressionType.COMBINED, "[[potion] effect [(with|by)]] %potioneffecttype% for %timespan% with [a] [tier [of]] %number% [particles %-boolean%[ with ambient [effect] %-boolean%[ and [particle] colo[u]r[ed] %-color%]]]]]");
+        registerExpression(ExprCustomPotionItem.class, ItemStack.class, ExpressionType.COMBINED, "[(0¦normal|1¦splash|2¦linger[ing])] potion (of|by|with|from) [effect[s]] %potioneffects%");
+        registerExpression(ExprEntityPotionEffects.class, PotionEffect.class, ExpressionType.COMBINED, "[(all|every|each)] [active] [potion] effects (on|in) %livingentities%", "[(every|all|each) of] %livingentities%['s] [active] [potion] effect[s]");
+        registerExpression(ExprPotionItemEffects.class, PotionEffect.class, ExpressionType.COMBINED, "[(all|every|each)] [potion] effect[s] (on|of) %itemstack%", "[(all|every|each) of] %itemstack%['s] [potion] effect[s]");
+        registerExpression(ExprPotionEffectType.class, PotionEffectType.class, ExpressionType.COMBINED, "potion[ ]effect[[ ]type][s] of %potioneffect%", "%potioneffect%['s] potion[ ]effect[[ ]type][s]");
+        registerExpression(ExprPotionEffectDuration.class, Timespan.class, ExpressionType.COMBINED, "(duration|length) of [potion] effect[s] %potioneffect%", "[potion] effect[s] %potioneffect%['s] (duration|length)");
+        registerExpression(ExprPotionEffectTier.class, Number.class, ExpressionType.COMBINED, "(tier|level|amplifier|power) of [potion] [effect] %potioneffect%", "[potion] [effect] %potioneffect%['s] (tier|amplifier|level|power)");
+    }
+
+
+    //Potions
+    //I know I could have used ternary operators, but it makes the code much less readable
+    public static PotionEffect fromPotionData(PotionData data) {
+        PotionEffectType type = data.getType().getEffectType();
+        if (type == PotionEffectType.HEAL || type == PotionEffectType.HARM) { //Instant potions
+            if (data.isUpgraded()) {
+                return new PotionEffect(type, 1, 2);
+            } else {
+                return new PotionEffect(type, 1, 1);
+            }
+        } else if (type == PotionEffectType.REGENERATION || type == PotionEffectType.POISON) { //Regen and poison potions have smaller durations
+            if (data.isExtended()) {
+                return new PotionEffect(type, 1800, 1);
+            } else if (data.isUpgraded()) {
+                return new PotionEffect(type, 440, 2);
+            } else {
+                return new PotionEffect(type, 900, 1);
+            }
+        } else if (type == PotionEffectType.NIGHT_VISION || type == PotionEffectType.INVISIBILITY || type == PotionEffectType.FIRE_RESISTANCE || type == PotionEffectType.WATER_BREATHING) { //Potions that don't have an upgraded version
+            if (data.isExtended()) {
+                return new PotionEffect(type, 9600, 1);
+            } else {
+                return new PotionEffect(type, 3600, 1);
+            }
+        } else if (type == PotionEffectType.WEAKNESS || type == PotionEffectType.SLOW) { //Those potions don't have an upgraded version AND have smaller durations
+            if (data.isExtended()) {
+                return new PotionEffect(type, 4800, 1);
+            } else {
+                return new PotionEffect(type, 1800, 1);
+            }
+        } else {
+            if (data.isExtended()) {
+                return new PotionEffect(type, 9600, 1);
+            } else if (data.isUpgraded()) {
+                return new PotionEffect(type, 1800, 2);
+            } else {
+                return new PotionEffect(type, 3600, 1);
+            }
+        }
+    }
+
+    public static PotionData emptyPotionData() {
+        return new PotionData(PotionType.WATER);
+    }
+    //Conventions
+    public static String getPrefix() {
+        return "[QuarSk] ";
+    }
+
+    public static QuarSk getInstance() {
+        return instance;
+    }
+
+    public static String getVersion() {
+        return "1.1";
+    }
     //All of the below functions are by bi0qaw cause I honestly can't do that kind of math. Thanks to you.
     public static Vector vectorFromLocations(Location from, Location to) {
         return new Vector(to.getX() - from.getX(), to.getY() - from.getY(), to.getZ() - from.getZ());
@@ -71,9 +201,8 @@ public class QuarSk extends JavaPlugin {
 
     public static void debug(Object... l) {
         for (Object o : l) {
-            Bukkit.getLogger().log(Level.INFO, "[QuarSk] " + o.toString());
+            Bukkit.getLogger().log(Level.INFO, getPrefix() + " " + o.toString());
         }
     }
 }
-
 
